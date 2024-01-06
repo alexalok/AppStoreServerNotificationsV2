bba71fb147824bc1d576878e6574151887595c5a
Alex Ch <aleks00799@gmail.com>
2024-01-06 13:41:21 +0300

    add an ability to get a decoded raw payload

--- AppStoreServerNotificationsV2/Models/ResponseBodyV2.cs
+++ AppStoreServerNotificationsV2/Models/ResponseBodyV2.cs
@@ -1,26 +1,30 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json.Serialization;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AppStoreServerNotificationsV2.Models;
 
 public record ResponseBodyV2([property: JsonPropertyName("signedPayload")] string SignedPayload)
 {
+    static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter() },
+    };
+
     public ResponseBodyV2DecodedPayload Decode()
     {
-        var opt = new JsonSerializerOptions()
-        {
-            PropertyNameCaseInsensitive = true,
-        };
-        opt.Converters.Add(new JsonStringEnumConverter());
+        var rawPayload = GetDecodedRawPayload();
+        var payload = JsonSerializer.Deserialize<ResponseBodyV2DecodedPayload>(rawPayload, SerializerOptions);
+        return payload!;
+    }
 
+    public string GetDecodedRawPayload()
+    {
         var handler = new JwtSecurityTokenHandler();
         var outerJwt = handler.ReadJwtToken(SignedPayload);
-
         var payloadJson = Base64UrlEncoder.Decode(outerJwt.RawPayload);
-        var payload = JsonSerializer.Deserialize<ResponseBodyV2DecodedPayload>(payloadJson, opt);
-
-        return payload!;
+        return payloadJson;
     }
 }

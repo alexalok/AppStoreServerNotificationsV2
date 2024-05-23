e602d8a1fade5c577e4ec937fae81aa04fd99cd5
Kej <vadim.ccram@gmail.com>
2024-05-23 10:56:30 +0300

    make SignedRenewalInfo optional

--- AppStoreServerNotificationsV2/Models/Data.cs
+++ AppStoreServerNotificationsV2/Models/Data.cs
@@ -6,4 +6,4 @@ namespace AppStoreServerNotificationsV2.Models;
 
 public record Data(string BundleId, string BundleVersion, Environment Environment,
     [property: JsonConverter(typeof(JWSTransactionDecodedPayloadConverter))] JWSTransactionDecodedPayload SignedTransactionInfo,
-    [property: JsonConverter(typeof(JWSRenewalInfoDecodedPayloadConverter))] JWSRenewalInfoDecodedPayload SignedRenewalInfo);
+    [property: JsonConverter(typeof(JWSRenewalInfoDecodedPayloadConverter))] JWSRenewalInfoDecodedPayload? SignedRenewalInfo);

77d564621c06b4c6d046c34f2160c9bf28c9b304
Aleksei Chistiakov <aleks00799@gmail.com>
2026-02-18 14:19:14 +0300

    make RevocationDate nullable

--- AppStoreServerNotificationsV2/Models/JWSTransactionDecodedPayload.cs
+++ AppStoreServerNotificationsV2/Models/JWSTransactionDecodedPayload.cs
@@ -5,4 +5,4 @@ public record JWSTransactionDecodedPayload(
     long OriginalTransactionId, OfferType? OfferType, string ProductId, long TransactionId, 
     long Price, string Currency, string Storefront, string Environment, bool IsUpgraded, string StorefrontId, 
     string InAppOwnershipType, string? AppAccountToken, string? AppTransactionId,
-    DateTimeOffset RevocationDate, int? RevocationPercentage, RevocationType? RevocationType);
+    DateTimeOffset? RevocationDate, int? RevocationPercentage, RevocationType? RevocationType);
